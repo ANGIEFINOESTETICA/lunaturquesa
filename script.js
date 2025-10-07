d343a40e4f3984bdaced756743853e61cecef015
ANGIEFINOESTETICA <angiefino.lunaturquesa@gmail.com>
2025-10-06 23:38:17 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('lang-toggle').addEventListener('click', function() {
-  document.querySelectorAll('.lang-es').forEach(e => e.classList.toggle('hidden'));
-  document.querySelectorAll('.lang-en').forEach(e => e.classList.toggle('hidden'));
-});
