7e647ec37a377c9bfd3ff388d4b85f55210c264a
ANGIEFINOESTETICA <angiefino.lunaturquesa@gmail.com>
2025-10-10 06:36:18 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.querySelectorAll('section').forEach(section => {
-  section.classList.add('hidden');
-});
-window.addEventListener('scroll', () => {
-  document.querySelectorAll('section').forEach(section => {
-    const rect = section.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      section.classList.add('visible');
-    }
-  });
-});
+document.addEventListener('DOMContentLoaded', ()=>{});
